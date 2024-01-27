3bdba74c104f7cbbf1e89fb0ac041ffcb9fa8538
adcerone <134092701+adcerone@users.noreply.github.com>
2024-01-27 20:15:01 -0300

    03
    *eliminados componentes y rutas de noticias en favor de sección de noticias estática

--- my-project/backend/router/index.js
+++ my-project/backend/router/index.js
@@ -1,11 +1,9 @@
 const mailRouter = require('./mail.router')
-const newsRouter = require('./news.router')
-const newsUploadRouter = require('./news.uploader.router')
+
 
 function apiRouter(server) {
     server.use('/mail', mailRouter)
-    server.use('/news', newsRouter)
-    server.use('/newsUpload', newsUploadRouter)
+
 }
 
 module.exports = apiRouter; 
