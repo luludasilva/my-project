e2dd5a0378eb4709b3df78205cd90c62765c7143
Lucia Dasilva <kineludasilva@gmail.com>
2024-07-10 18:05:35 -0300

    cambios finales

--- my-project/src/Components/Banner.jsx
+++ my-project/src/Components/Banner.jsx
@@ -15,26 +15,25 @@ const Banner = () => {
 
     
     return (
-        <div className="lg:px-56 px-10 lg:py-0 py-10 text-center gap-20 lg:text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-50 items-center">
-            <div className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white">
-                <h1 data-aos="fade-right"className="text-[70px] font-semibold mb-8 leading-normal uppercase">Bienvenidos a <span className="text-teal-500 "> INTEGRA SPORTS</span></h1>
-                <p data-aos="fade-right" className="text-justify">Somos una escuela multideportiva dirigida exclusivamente a niños y adolescentes con discapacidad. Formada por Profesores de Educación Física, que trabajan de forma independiente y en diferentes instituciones, brindando iniciación en actividades acuáticas y multiderportivas. <br />
-                Nuestra misión: Mejorar los aspectos físicos, sociales, afectivos y psiquicos de niños y adolescentes con discapacidad, procurando contribuir a su formación integral y aportando desde nuestra área, la eduacion física, herramientas para que logren desenvolverse de la forma más autónoma posible en la sociedad y de esta forma puedan alcanzar una plena integración social, educativa, cultural y laboral. 
-                </p>
+        <div className="lg:px-10 px-10 lg:py-0 py-10 text-center gap-5 lg:text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-28 items-center">
+            <img data-aos="fade-up" src={img} width={800} height={800} className="rounded-full border-2 p-1 border-emerald-600 img_glow" alt=""/>
+            <div className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white text-justify">
+                <h1 data-aos="fade-right" className="text-[52px] font-semibold mb-8 leading-normal uppercase  text-justify justify-center">Bienvenidos a < div className="text-teal-500"> INTEGRA SPORTS</div></h1>
+                <p data-aos="fade-right">Somos una escuela multideportiva dirigida exclusivamente a niños y adolescentes con discapacidad. Formada por Profesores de Educación Física, que trabajan de forma independiente y en diferentes instituciones, brindando iniciación en actividades acuáticas y multiderportivas.</p>
+                <p data-aos="fade-right">Nuestra misión: Mejorar los aspectos físicos, sociales, afectivos y psíquicos de niños y adolescentes con discapacidad, procurando contribuir a su formación integral y aportando desde nuestra área, la educación física, herramientas para que logren desenvolverse de la forma más autónoma posible en la sociedad y de esta forma puedan alcanzar una plena integración social, educativa, cultural y laboral.</p>
                 <div className="flex mt-8 gap-2">
                     <div className="flex items-center justify-center">
                         <div className="flex space-x-2">
-                            <a href="https://www.instagram.com/integrasportsmdp/" >
-                            <FaInstagram className="text-[35px] rounded-full p-1 glow"/>
+                            <a href="https://www.instagram.com/integrasportsmdp/">
+                                <FaInstagram className="text-[35px] rounded-full p-1 glow"/>
                             </a>
                             <a href="https://www.facebook.com/integrasports?fref=ts">
-                            <FaFacebook className="text-[35px] rounded-full p-1 glow" />
+                                <FaFacebook className="text-[35px] rounded-full p-1 glow" />
                             </a>
                         </div>
                     </div>
                 </div>
             </div>
-            <img data-aos="fade-up" src={img} width={290} height={290} className="rounded-full border-2 p-1 border-emerald-600 img_glow " alt=""/>
         </div>
     );
 };
--- my-project/src/Components/Footer.jsx
+++ my-project/src/Components/Footer.jsx
@@ -1,35 +1,34 @@
 import { FaInstagram } from "react-icons/fa";
 import { FaFacebook } from 'react-icons/fa';
 import img from '../assets/isologotipoblanco.png';
+
 const Footer = () => {
     return (
-        <footer className='bg-cyan-950 text-white lg:px-8 px-4 py-8'>
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-                <div className='text-center md:text-left'>
-                    <span className='text-2xl font-semibold text-emerald-600 py-2 uppercase'>
-                        <img src={img} height={200} width={200} className="object-cover mx-auto md:mx-0" alt="Logo" />
-                    </span>
+        <footer className='bg-cyan-950 text-white lg:px-20 px-4 py-10'>
+            <div className='container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8'>
+                <div className='mb-4 md:mb-0 flex justify-center md:justify-start'>
+                    <img src={img} height={200} width={200} className="object-cover" alt="Logo" />
                 </div>
                 <div>
-                    <h2 className='text-lg font-semibold text-emerald-600 py-2 uppercase'>Ubicación</h2>
-                    <ul className='text-sm my-4'>
-                        <li className='my-1'>Escuela multideportiva para personas con discapacidad</li>
-                        <li className='my-1'>Mar del Plata</li>
-                        <li className='my-1'>Argentina</li>
+                    <h2 className='text-[22px] font-semibold text-emerald-600 py-2 uppercase'>Ubicación</h2>
+                    <ul className='text-[16px] my-4'>
+                        <li className='my-2'>Escuela multideportiva para personas con discapacidad</li>
+                        <li className='my-2'>Mar del Plata</li>
+                        <li className='my-2'>Argentina</li>
                     </ul>
                 </div>
-                <div>
-                    <h2 className='text-lg font-semibold text-emerald-600 py-2 uppercase'>¡Sumate como Sponsor!</h2>
-                    <p className='text-sm my-4'>Email: integra-sports@hotmail.com</p>
+                <div className='mb-4 md:mb-0'>
+                    <h2 className='text-[22px] font-semibold text-emerald-600 py-2 uppercase'>¡Sumate como Sponsor!</h2>
+                    <p className='text-[16px] my-4'>Email: integra-sports@hotmail.com</p>
                 </div>
-                <div>
-                    <h2 className='text-lg font-semibold text-emerald-600 py-2 uppercase'>Seguinos</h2>
-                    <div className="flex space-x-4">
+                <div className='mb-4 md:mb-0'>
+                    <h2 className='text-[22px] font-semibold text-emerald-600 py-2 uppercase'>Seguinos</h2>
+                    <div className="flex justify-center md:justify-start space-x-4">
                         <a href="https://www.instagram.com/integrasportsmdp/">
-                            <FaInstagram className="text-lg rounded-full p-1 glow" />
+                            <FaInstagram className="text-[35px] rounded-full p-1 glow"/>
                         </a>
                         <a href="https://www.facebook.com/integrasports?fref=ts">
-                            <FaFacebook className="text-lg rounded-full p-1 glow" />
+                            <FaFacebook className="text-[35px] rounded-full p-1 glow" />
                         </a>
                     </div>
                 </div>
